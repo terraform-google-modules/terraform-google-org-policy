c7b650ab56f6efb4434b8423791f75f292da4db9
Ivan Kornienko <ivan.kornienko@gmail.com>
2019-10-01 17:51:42 +0300

    Migrated to Cloud Build

---
+++ test/setup/iam.tf
@@ -0,0 +1,85 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+locals {
+  project_roles = [
+    "roles/owner"
+  ]
+
+  organization_roles = [
+    "roles/orgpolicy.policyAdmin"
+  ]
+
+  folders = [
+    "1_0d419896c29",
+    "2_0d419896c29",
+  ]
+}
+
+resource "google_folder" "org_policy" {
+  for_each = toset(local.folders)
+
+  display_name = "ci-org-policy-${each.value}"
+  parent       = "folders/${var.folder_id}"
+}
+
+
+resource "google_service_account" "int_test" {
+  project      = module.project.project_id
+  account_id   = "ci-account"
+  display_name = "ci-account"
+}
+
+resource "google_project_iam_member" "int_test_project" {
+  for_each = toset(local.project_roles)
+
+  project = module.project.project_id
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.int_test.email}"
+}
+
+resource "google_project_iam_member" "int_test_project_exclude" {
+  for_each = toset(local.project_roles)
+
+  project = module.project_exclude.project_id
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.int_test.email}"
+}
+
+resource "google_organization_iam_member" "int_test" {
+  for_each = toset(local.organization_roles)
+
+  org_id = var.org_id
+  role   = each.value
+  member = "serviceAccount:${google_service_account.int_test.email}"
+}
+
+resource "google_service_account_key" "int_test" {
+  service_account_id = google_service_account.int_test.id
+}
+
+resource "null_resource" "wait_permissions" {
+  # Adding a pause as a workaround for of the provider issue
+  # https://github.com/terraform-providers/terraform-provider-google/issues/1131
+  provisioner "local-exec" {
+    command = "echo sleep 30s for permissions to get granted; sleep 30"
+  }
+  depends_on = [
+    google_organization_iam_member.int_test,
+    google_project_iam_member.int_test_project,
+    google_project_iam_member.int_test_project_exclude
+  ]
+}
---
+++ test/setup/outputs.tf
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+output "project_id" {
+  value = module.project.project_id
+}
+
+output "project_exclude_id" {
+  value = module.project_exclude.project_id
+}
+
+output "folder_1_id" {
+  value = google_folder.org_policy[local.folders[0]].id
+}
+
+output "folder_2_id" {
+  value = google_folder.org_policy[local.folders[1]].id
+}
+
+output "sa_key" {
+  value     = google_service_account_key.int_test.private_key
+  sensitive = true
+}
